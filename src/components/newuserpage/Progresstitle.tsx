9789d6cf21d4b348561201832be7bebd2202ab8b
姜港 <890060@leapmotor.com>
2025-04-29 14:54:05 +0800

    init Commit

--- src/components/newuserpage/Progresstitle.tsx
+++
@@ -1,9 +0,0 @@
-
-
-function Progresstitle() {
-    return (
-        <h2 id='signupTitle' className='font-medium text-xl animate-[fadeInUp] duration-300 block'>Welcome, select an account type.</h2>
-    )
-}
-
-export default Progresstitle
